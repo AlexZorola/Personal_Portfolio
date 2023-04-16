976e508d22b926ce8ed77e02d850fb05fbc3a003
Alex Zorola <alex.zorola01@gmail.com>
2023-04-16 12:12:30 -0500

    final commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF, FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+
+const SocialMedia = () => (
+
+  <div className="app__social">
+    <div>
+      <a href='https://twitter.com/ChilledAlex97'><BsTwitter /></a>
+    </div>
+    <div>
+      <a href='https://www.linkedin.com/in/alex-zorola-b2b220247/'><FaLinkedin /></a>
+    </div>
+    <div>
+      <a href='https://www.instagram.com/chilledalex97/'><BsInstagram /></a>
+    </div>
+    <div>
+      <a href='https://github.com/AlexZorola'><FaGithub /></a>
+    </div>
+    <div>
+      <a href='https://www.youtube.com/channel/UCJgkOQkGmZva27BOJ-TVBRA'><FaYoutube /></a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
---
+++ src/constants/images.js
@@ -0,0 +1,82 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import sql from '../assets/sql.png';
+import unreal5 from '../assets/unreal5.png';
+import teamwork from '../assets/teamwork.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import spotify from '../assets/spotify.png';
+import discord from '../assets/discord.png';
+import tft from '../assets/tft.png';
+import csgo from '../assets/csgo.png';
+import btd from '../assets/btd.png';
+import naruto from '../assets/naruto.png';
+import konosuba from '../assets/konosuba.png';
+import onepiece from '../assets/onepiece.png';
+import twitch from '../assets/twitch.png';
+import gt from '../assets/GT.png'
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  html,
+  javascript,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  sql,
+  unreal5,
+  teamwork,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  spotify,
+  discord,
+  tft,
+  csgo,
+  btd,
+  naruto,
+  konosuba,
+  onepiece,
+  twitch,
+  gt,
+};
